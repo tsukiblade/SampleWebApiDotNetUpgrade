f1c042d466407c09a33706b1826b681b8d457740
Szymon Kozłowski <szl@spyro-soft.com>
2023-02-27 22:37:45 +0100

    changes

--- src/SampleWebApi/Controllers/WeatherForecastController.cs
+++ src/SampleWebApi/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using SampleWebApi.Models;
 
 namespace SampleWebApi.Controllers
 {
--- src/SampleWebApi/Models/WeatherForecast.cs
+++ src/SampleWebApi/Models/WeatherForecast.cs
@@ -1,4 +1,4 @@
-namespace SampleWebApi
+namespace SampleWebApi.Models
 {
     public class WeatherForecast
     {
