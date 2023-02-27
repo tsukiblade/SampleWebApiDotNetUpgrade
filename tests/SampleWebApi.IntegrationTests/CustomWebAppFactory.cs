21e672af20c5116d3005e4c3b4bf3eff948ebf77
Szymon Kozłowski <szl@spyro-soft.com>
2023-02-27 22:47:12 +0100

    changes

---
+++ tests/SampleWebApi.IntegrationTests/CustomWebAppFactory.cs
@@ -0,0 +1,60 @@
+﻿using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Configurations;
+using DotNet.Testcontainers.Containers;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SampleWebApi.Models;
+
+namespace SampleWebApi.IntegrationTests
+{
+    //In memory version of web api
+    public class CustomWebAppFactory<TProgram> : WebApplicationFactory<TProgram>
+        where TProgram : class
+    {
+        private readonly int _port = Random.Shared.Next(10000, 50000);
+        private readonly MsSqlTestcontainer _dbContainer;
+
+        public CustomWebAppFactory()
+        {
+            _dbContainer = new ContainerBuilder<MsSqlTestcontainer>()
+                .WithDatabase(new MsSqlTestcontainerConfiguration
+                {
+                    Password = "Pass@word",
+                    Port = _port
+                })
+                .WithPortBinding(_port, 1433)
+                .WithName($"MsSqlTestContainer{Guid.NewGuid()}")
+                .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(1433))
+                .Build();
+        }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureTestServices(services =>
+            {
+                var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<TodoContext>));
+
+                if (descriptor != null)
+                {
+                    services.Remove(descriptor);
+                }
+
+                services.AddDbContext<TodoContext>(opt => opt.UseSqlServer(_dbContainer.ConnectionString));
+            });
+        }
+
+        public async Task Init()
+        {
+            await _dbContainer.StartAsync();
+        }
+
+        public override ValueTask DisposeAsync()
+        {
+            _dbContainer.DisposeAsync();
+            return base.DisposeAsync();
+        }
+    }
+}
---
+++ tests/SampleWebApi.IntegrationTests/TodoItemsControllerTests.cs
@@ -0,0 +1,62 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SampleWebApi.Models;
+
+namespace SampleWebApi.IntegrationTests
+{
+    [TestFixture]
+    public class TodoItemsControllerTests
+    {
+        private readonly CustomWebAppFactory<Program> _webApplicationFactory;
+
+        private HttpClient SystemUnderTest => _webApplicationFactory.CreateClient();
+
+        public TodoItemsControllerTests()
+        {
+            _webApplicationFactory = new CustomWebAppFactory<Program>();
+        }
+
+        [SetUp]
+        public async Task SetUp()
+        {
+            await _webApplicationFactory.Init();
+        }
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            await _webApplicationFactory.DisposeAsync();
+        }
+
+        [Test]
+        [AutoData]
+        public async Task Create_ToDoItem_WhenDataIsValid(TodoItem todoItem)
+        {
+            // Arrange
+            todoItem.Id = 0;
+
+            // Act
+            var response = await SystemUnderTest.PostAsJsonAsync("x", todoItem);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.Created);
+        }
+
+        [Test]
+        [AutoData]
+        public async Task Create_ToDoItem_WhenIdIsInvalid(TodoItem todoItem)
+        {
+            // Arrange
+            todoItem.Id = -999;
+
+            // Act
+            var response = await SystemUnderTest.PostAsJsonAsync("x", todoItem);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.InternalServerError);
+        }
+    }
+}
